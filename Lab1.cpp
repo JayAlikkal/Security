a516b9c0c65e7ee10a9a565192d50660fd78e760
JayAlikkal <60823882+JayAlikkal@users.noreply.github.com>
2023-09-07 23:50:51 -0400

    Update Lab1.cpp
    added graph

--- Lab1.cpp
+++ Lab1.cpp
@@ -13,7 +13,7 @@ int main() {
     }
 
     const int byteCount = 256;
-    int byteFrequency[byteCount] = {0};
+    int byteFrequency[byteCount] = { 0 };
 
     char byte;
     while (file.get(byte)) {
@@ -22,18 +22,47 @@ int main() {
 
     file.close();
 
-    cout << "Byte frequencies in the file:" << endl;
+    int maxFrequency = 0;
+    for (int i = 0; i < byteCount; ++i) {
+        if (byteFrequency[i] > maxFrequency) {
+            maxFrequency = byteFrequency[i];
+        }
+    }
+
+    // Print the top border
+    cout << "+";
+    for (int i = 0; i < byteCount * 2; ++i) {
+        cout << "-";
+    }
+    cout << "+" << endl;
+
+    for (int row = maxFrequency; row > 0; --row) {
+        cout << "|";
+        for (int i = 0; i < byteCount; ++i) {
+            if (byteFrequency[i] >= row) {
+                cout << "# ";
+            }
+            else {
+                cout << "  "; // Two spaces for characters without frequency
+            }
+        }
+        cout << "|" << endl;
+    }
+
+    // Print the bottom border
+    cout << "+";
+    for (int i = 0; i < byteCount * 2; ++i) {
+        cout << "-";
+    }
+    cout << "+" << endl;
+
+    // Print characters on the x-axis
+    cout << "|";
     for (int i = 0; i < byteCount; ++i) {
         char character = static_cast<char>(i);
-        cout << character << ": " << byteFrequency[i] << " | ";
-
-        // Display the frequency as vertical bars
-        for (int j = 0; j < byteFrequency[i]; ++j) {
-            cout << "#";
-        }
-
-        cout << endl;
+        cout << character << ' ';
     }
+    cout << "|" << endl;
 
     return 0;
 }
