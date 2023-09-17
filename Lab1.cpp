8e2ed916b547fcdb1c1042841c7f4c91c52180f5
JayAlikkal <60823882+JayAlikkal@users.noreply.github.com>
2023-09-17 00:35:29 -0400

    security final

--- Lab1.cpp
+++ Lab1.cpp
@@ -6,8 +6,68 @@ using namespace std;
 
 const int byteCount = 256;
 
+void PrintCharacter() {
+    // Print characters on the x-axis
+    cout << "|" << endl;
+    for (int i = 0; i < byteCount; ++i)
+    {
+        char character = static_cast<char>(i);
+        if (character > 32 && character < 127) 
+        {
+            cout << character;
+        }
+        else {
+            cout << " ";
+        }
+    }
+    cout << "|" << endl;
+    cout << endl;
+}
+
+void printAllASCIICharacters() {
+    int counter = 0;
+
+    cout << "|" << endl;
+    for (int i = 0; i <= 255; ++i) {
+        // Calculate ASCII value digits
+        int hundreds = i / 100;
+        if (hundreds == 0) {
+            cout << " ";
+        }
+        else {
+            std::cout << hundreds;
+        }
+
+    }cout << "|" << endl; cout << endl;
+
+    cout << "|" << endl;
+    for (int i = 0; i <= 255; ++i) {
+        // Calculate ASCII value digits
+        int tens = (i / 10) % 10;
+
+        // Print hundreds, tens, and ones vertically
+        if (i < 10) {
+            cout << " ";
+        }
+        else {
+            std::cout << tens;
+        }
+    }cout << "|" << endl; cout << endl;
+
+    cout << "|" << endl;
+    for (int i = 0; i <= 255; ++i) {
+        // Calculate ASCII value digits
+        int ones = i % 10;
+
+        // Print hundreds, tens, and ones vertically
+        std::cout << ones;
+        counter++;
+    }cout << "|" << endl; cout << endl;
+
+}
+
 // Function to generate the histogram
-void generateHistogram(const int byteFrequency[], int ratio) {
+void generateHistogram(const int byteFrequency[], int ratio, bool lflag, bool nflag, bool aflag) {
     int maxFrequency = 0;
     for (int i = 0; i < byteCount; ++i) {
         if (byteFrequency[i] > maxFrequency) {
@@ -16,7 +76,7 @@ void generateHistogram(const int byteFrequency[], int ratio) {
     }
 
     // Calculate the scaling factor
-    double scale = (maxFrequency > 0) ? static_cast<double>(ratio) / maxFrequency : 1.0;
+    double scale = (lflag) ? static_cast<double> ((log)(maxFrequency) / ratio) : static_cast<double> (maxFrequency / (ratio));
 
     // Print the top border
     cout << "+";
@@ -28,7 +88,7 @@ void generateHistogram(const int byteFrequency[], int ratio) {
     for (int row = ratio; row > 0; --row) {
         cout << "|";
         for (int i = 0; i < byteCount; ++i) {
-            int scaledValue = static_cast<int>(byteFrequency[i] * scale);
+            int scaledValue = (lflag) ? static_cast<int>(ceil(log(byteFrequency[i]) / scale)) : static_cast<int>(ceil(byteFrequency[i] / scale));
             if (scaledValue >= row) {
                 cout << "* ";
             }
@@ -46,111 +106,68 @@ void generateHistogram(const int byteFrequency[], int ratio) {
     }
     cout << "+" << endl;
 
+    if (nflag)
+    {
+        PrintCharacter();
+    }
+
+    if (aflag)
+    {
+        printAllASCIICharacters();
+    }
 
 }
 
 
-void PrintCharacter() {
-    // Print characters on the x-axis
-    for (int i = 0; i < byteCount; ++i) {
-        char character = static_cast<char>(i);
-        if (character > 33 && character < 127) {
-            cout << character;
-        }
-        else {
-            cout << " ";
-        }
-    }
-    cout << endl;
-}
-
-
-void printAllASCIICharacters() {
-    int counter = 0;
-    for (int i = 0; i <= 255; ++i) {
-        // Calculate ASCII value digits
-        int hundreds = i / 100;
-        if (hundreds == 0) {
-            cout << " ";
-        }
-        else {
-            std::cout << hundreds;
-        }
-       
-    }cout << endl;
-    for (int i = 0; i <= 255; ++i) {
-        // Calculate ASCII value digits
-        int tens = (i / 10) % 10;
-
-        // Print hundreds, tens, and ones vertically
-        if (i < 10) {
-            cout << " ";
-        }
-        else {
-            std::cout << tens;
-        }
-    }cout << endl;
-    for (int i = 0; i <= 255; ++i) {
-        // Calculate ASCII value digits
-        int ones = i % 10;
-
-        // Print hundreds, tens, and ones vertically
-        std::cout << ones;
-        counter++;
-    }cout << endl;
-    
-}
-
-
-void generateLogHistogram(const int byteFrequency[], int ratio) {
-    int maxFrequency = 0;
-    for (int i = 0; i < byteCount; ++i) {
-        if (byteFrequency[i] > maxFrequency) {
-            maxFrequency = byteFrequency[i];
-        }
-    }
-
-    // Calculate the maximum log frequency
-    double maxLogFrequency = 0.0;
-    for (int i = 0; i < byteCount; ++i) {
-        double logValue = log(static_cast<double>(byteFrequency[i]) + 1.0); // Adding 1 to avoid log(0)
-        if (logValue > maxLogFrequency) {
-            maxLogFrequency = logValue;
-        }
-    }
-
-    // Calculate the scaling factor based on the maximum log frequency
-    double scale = (maxLogFrequency > 0.0) ? static_cast<double>(ratio) / maxLogFrequency : 1.0;
-
-    // Print the top border
-    cout << "+";
-    for (int i = 0; i < byteCount * 2; ++i) {
-        cout << "-";
-    }
-    cout << "+" << endl;
-
-    for (int row = ratio; row > 0; --row) {
-        cout << "|";
-        for (int i = 0; i < byteCount; ++i) {
-            double logValue = log(static_cast<double>(byteFrequency[i]) + 1.0);
-            int scaledValue = static_cast<int>(logValue * scale);
-            if (scaledValue >= row) {
-                std::cout << "* ";
-            }
-            else {
-                cout << "  ";
-            }
-        }
-        cout << "|" << endl;
-    }
-
-    // Print the bottom border
-    cout << "+";
-    for (int i = 0; i < byteCount * 2; ++i) {
-        cout << "-";
-    }
-   cout << "+" << endl;
-}
+//void generateLogHistogram(const int byteFrequency[], int ratio) {
+//    int maxFrequency = 0;
+//    for (int i = 0; i < byteCount; ++i) {
+//        if (byteFrequency[i] > maxFrequency) {
+//            maxFrequency = byteFrequency[i];
+//        }
+//    }
+//
+//    // Calculate the maximum log frequency
+//    double maxLogFrequency = 0.0;
+//    for (int i = 0; i < byteCount; ++i) {
+//        double logValue = log(static_cast<double>(byteFrequency[i]) + 1.0); // Adding 1 to avoid log(0)
+//        if (logValue > maxLogFrequency) {
+//            maxLogFrequency = logValue;
+//        }
+//    }
+//
+//    // Calculate the scaling factor based on the maximum log frequency
+//    double scale = (maxLogFrequency > 0.0) ? static_cast<double>(ratio) / maxLogFrequency : 1.0;
+//
+//    // Print the top border
+//    cout << "+";
+//    for (int i = 0; i < byteCount * 2; ++i) {
+//        cout << "-";
+//    }
+//    cout << "+" << endl;
+//
+//    for (int row = ratio; row > 0; --row) {
+//        cout << "|";
+//        for (int i = 0; i < byteCount; ++i) {
+//            double logValue = log(static_cast<double>(byteFrequency[i]) + 1.0);
+//            int scaledValue = static_cast<int>(logValue * scale);
+//            if (scaledValue >= row) {
+//                std::cout << "* ";
+//            }
+//            else {
+//                cout << "  ";
+//            }
+//        }
+//        cout << "|" << endl;
+//    }
+//
+//    // Print the bottom border
+//    cout << "+";
+//    for (int i = 0; i < byteCount * 2; ++i) {
+//        cout << "-";
+//    }
+//   cout << "+" << endl;
+//}
 
 
 int main(int argc, char* argv[]) {
@@ -205,29 +222,31 @@ int main(int argc, char* argv[]) {
 
     file.close();
 
-    if (lflag && rflag) {
-        // Set the scale of histogram
-        generateLogHistogram(byteFrequency, ratio);
+    //if (lflag) {
+    //    // Set the scale of histogram
+    //    generateHistogram(byteFrequency, ratio, lflag);
+    //}
+    //else if (rflag) {
+    //    generateHistogram(byteFrequency, ratio, lflag);
+    //}
+    //if (gflag) {
+    //    // show no graph
+    //    cout << "stat";
+    //}
+    //if (nflag) {
+    //    // shows 0-255 along the X-axis (in three rows)
+    //    printAllASCIICharacters();
+    //}
+    //if (aflag) {
+    //    // shows ASCII characters (if printable) along the X-axis
+    //    PrintCharacter();
+    //}
+    //cout << endl;
+
+    if (gflag == false)
+    {
+        generateHistogram(byteFrequency, ratio, lflag, nflag, aflag);
     }
-    else if (lflag) {
-        generateLogHistogram(byteFrequency, ratio);
-    }
-    else if (rflag) {
-        generateHistogram(byteFrequency, ratio);
-    }
-    if (gflag) {
-        // show no graph
-        cout << "stat";
-    }
-    if (nflag) {
-        // shows 0-255 along the X-axis (in three rows)
-        printAllASCIICharacters();
-    }
-    if (aflag) {
-        // shows ASCII characters (if printable) along the X-axis
-        PrintCharacter();
-    }
-    cout << endl;
 
     int minFrequency = INT_MAX; // Initialize to a large value
 
